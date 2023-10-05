72aaeabfea2634b735adef3f8ac7459f250f84e8
SPM <145341960+spm-2020@users.noreply.github.com>
2023-10-05 21:46:44 +0530

    routing files added

---
+++ src/routing.tsx
@@ -0,0 +1,14 @@
+export const Routing = ()=>{
+    return (
+        <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    )
+}
