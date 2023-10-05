bfdf7a52c5e933710ef58a834d3ab5d611cb4923
Mohan SPM <40892986+cloudstarmohan@users.noreply.github.com>
2023-10-05 21:48:13 +0530

    routing layout added

--- src/routing.tsx
+++ src/routing.tsx
@@ -1,3 +1,11 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
+
 export const Routing = ()=>{
     return (
         <BrowserRouter>
